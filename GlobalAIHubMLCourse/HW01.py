aa62c7d107416ae6488f0b15779e3dcd4cb925fd
themohal <farjadmohal@gmail.com>
2021-01-05 02:02:19 +0500

    first hw01 in globalai

---
+++ GlobalAIHubMLCourse/HW01.py
@@ -0,0 +1,7 @@
+#Explain your work
+#this is for loop which will iterate a number where 'a' can be any number.
+#if a is equal 10 this will print 10 for 10 times.
+a=10
+#Question 1
+for x in range(a):
+    print(a)
